eb356cb5bd342f9e6c50db814b5dcd4207016760
phatyou <jen.phatmandu7@gmail.com>
2017-10-31 13:25:05 -0600

    commented out one line

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-connecting to MongoDB
+// connecting to MongoDB
 // mongoose.connect('mongodb://heroku_jxkjhg1v:6s68tem51mlionrj2sneb7b53c@ds127988.mlab.com:27988/heroku_jxkjhg1v');
 mongoose.connect('mongodb://heroku_v2stl7h1:jgt3pt20iq7in7p1dld1nticu5@ds237855.mlab.com:37855/heroku_v2stl7h1');
 
